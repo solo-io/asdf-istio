57f6936d9ec7025c3486e1316011b7140712f390
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-07-01 14:59:43 +0530

    set executable

--- lib/commands/command-set-istio-home.bash
+++ lib/commands/command-set-istio-home.bash
@@ -2,13 +2,18 @@ set -o pipefail
 
 MY_SHELL=$(basename "$MY_SHELL")
 
+compute_istio_home(){
+local istioctl_path
+istioctl_path="$(asdf which istioctl)"
+local istioctl_bin
+istioctl_bin="$( cd -P "$( dirname "$istioctl_path" )" && pwd )"
+istio_home="$(dirname "$istioctl_bin")"
+echo "$istio_home" 
+}
 case "$MY_SHELL" in
 'fish')
  ;;
 *)
-istioctl_path="$(asdf which istioctl)"
-export ISTIO_HOME
-ISTIOCTL_BIN="$( cd -P "$( dirname "$istioctl_path" )" && pwd )"
-ISTIO_HOME="$(dirname "$ISTIOCTL_BIN")"
+export ISTIO_HOME="$(compute_istio_home)"
 ;;
 esac
