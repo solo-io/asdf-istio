14e347d7e4d04b52264854cc4ec445b64224e4f9
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-07-01 15:09:42 +0530

    adding debug logs

--- lib/commands/command-set-istio-home.bash
+++ lib/commands/command-set-istio-home.bash
@@ -1,13 +1,16 @@
 set -o pipefail
+set +x
 
 MY_SHELL=$(basename "$MY_SHELL")
 
 zsh_compute_istio_home() {
+  echo "2"
   local istioctl_path
   istioctl_path="$(asdf which istioctl)"
   if [[ -n "${istioctl_path}" ]]; then
     export ISTIO_HOME
     ISTIO_HOME="$(dirname "$(dirname "${istioctl_path:A}")")"
+    echo "3"
   fi
 }
 
@@ -24,6 +27,7 @@ case "$MY_SHELL" in
 'fish')
  ;;
 'zsh')
+  echo "1"
   autoload -U add-zsh-hook
   add-zsh-hook precmd zsh_compute_istio_home
  ;;
