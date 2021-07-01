af9986810986190ad4778b390afb58b5c83ce0bc
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-07-01 14:24:44 +0530

    refactor set home function

--- lib/commands/command.bash
+++ lib/commands/command.bash
@@ -9,18 +9,24 @@ COMMANDS
    help             -   Show this help
 "
 
-function _asdf_istio_absolute_dir_path {
+_asdf_istio_absolute_dir_path() {
     local absolute_path
     absolute_path="$( cd -P "$( dirname "$1" )" && pwd )"
     echo "$absolute_path"
 }
 
-function set_istio_home() {
+set_istio_home() {
   local istioctl_path
   istioctl_path="$(asdf which istioctl)"
+  ISTIO_HOME_PATH="$(_asdf_istio_absolute_dir_path "${istioctl_path}")"
   if [[ -n "${istioctl_path}" ]]; then
-    export ISTIO_HOME
-    ISTIO_HOME="$(dirname "$(_asdf_istio_absolute_dir_path "${istioctl_path}")")"
+    case "$MY_SHELL" in 
+    'fish')
+      $SHELL set -gx ISTIO_HOME "$ISTIO_HOME_PATH"
+      ;;
+    *)
+    export ISTIO_HOME="$ISTIO_HOME_PATH"
+    esac
   fi
 }
 
