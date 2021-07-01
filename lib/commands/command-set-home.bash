e2106a8dcdcf331de9310ab44623df099bd653e6
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-07-01 16:49:46 +0530

    more debugs

--- lib/commands/command-set-home.bash
+++ lib/commands/command-set-home.bash
@@ -9,4 +9,18 @@ compute_istio_home(){
   echo "Set ISTIO_HOME to $ISTIO_HOME"
 }
 
+MY_SHELL=$(basename "$MY_SHELL")
+echo "My Shell is $MY_SHELL"
+
+case "$MY_SHELL" in
+'fish')
+ ;;
+'zsh')
+  autoload -U add-zsh-hook
+  add-zsh-hook precmd compute_istio_home
+ ;;
+*)
 compute_istio_home
+;;
+esac 
+
